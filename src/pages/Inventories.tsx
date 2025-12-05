a1a4e79c8663340a4fccd44af19dd5008a732536
alexwithstars <rayo080378@gmail.com>
2025-12-04 19:38:09 -0600

    feat(Inventories): inventories page implementation

--- src/pages/Inventories.tsx
+++ src/pages/Inventories.tsx
@@ -1,12 +1,55 @@
-import { ReactElement } from 'react'
+import { ReactElement, useState } from 'react'
 import './Inventories.css'
 import { Navbar } from '../components/Navbar'
+import { ModeSelector, type Mode } from '@/components/Inventories/ModeSelector'
+import { IconBox, IconDiscount2Off, IconStack } from '@tabler/icons-react'
+import { InventoriesProvider } from '@/contexts/Inventories'
+import { Output } from '@/components/Inventories/Output'
+import { InputDiscount, InputEOQ, InputProduction } from '@/components/Inventories/Input'
+
+const modes: Array<{
+  element: React.FC
+  mode: Mode
+}> = [
+  {
+    element: InputEOQ,
+    mode: { name: 'Lote Económico', icon: IconBox }
+  },
+  {
+    element: InputDiscount,
+    mode: { name: 'Descuento por Cantidad', icon: IconDiscount2Off }
+  },
+  {
+    element: InputProduction,
+    mode: { name: 'Lote de Producción', icon: IconStack }
+  }
+]
 
 export function Inventories (): ReactElement {
+  const [selectedMode, setSelectedMode] = useState(0)
+
+  const SelectedElement = modes[selectedMode].element
+
   return (
-    <main className='inventories-container'>
-      <Navbar />
-      <h1>Inventories Page</h1>
-    </main>
+    <InventoriesProvider>
+      <main className='inventories-container'>
+        <Navbar
+          title='Inventarios'
+          backgroundColor='var(--primary800)'
+          color='var(--primary300)'
+        />
+        <section className='mode-selector-section'>
+          <ModeSelector options={modes.map(mode => mode.mode)} onSelect={setSelectedMode} />
+        </section>
+        <section className='inventories-content-section'>
+          <div className='inventories-content-card'>
+            <SelectedElement />
+          </div>
+          <div className='inventories-content-card'>
+            <Output />
+          </div>
+        </section>
+      </main>
+    </InventoriesProvider>
   )
 }
